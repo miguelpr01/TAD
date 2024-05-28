7de2aa9d9c10ec4bd79c862caa03fe35eeb017c9
rodrikk <rodrik33.2002@gmail.com>
2024-05-28 17:43:54 +0200

    feat: creative commons

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -96,7 +96,7 @@
             </div>
             <div class="row">
                 <div class="col-md-12 text-center copyright">
-                    <p>&copy; {{__('messages.copyright')}}</p>
+                <p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="https://github.com/miguelpr01/TAD">Naturfit</a> by <span property="cc:attributionName">Miguel Pavón Rodríguez, Daniel “Polaco” Martínez Krol, Rodrigo Bautista Hernández</span> is licensed under <a href="https://creativecommons.org/licenses/by-nd/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-ND 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nd.svg?ref=chooser-v1" alt=""></a></p>
                 </div>
             </div>
         </div>
